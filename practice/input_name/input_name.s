e62c2f58df0819230f179fcb7892a00f29cf79db
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-22 15:50:37 -0600

    Added calling convention for registers preservation

--- practice/input_name/input_name.s
+++ practice/input_name/input_name.s
@@ -12,39 +12,39 @@ new_line: .asciz "\n"
 _start:
 
     # Write the prompt "Enter input: " to stdout
-    movq $1, %rax            # syscall: sys_write (1)
-    movq $1, %rdi            # file descriptor: stdout (1)
-    movq $prompt_msg, %rsi          # address of the message to write
-    movq prompt_length, %rdx           # number of bytes to write (length of the message)
+    mov $1, %rax            # syscall: sys_write (1)
+    mov $1, %rdi            # file descriptor: stdout (1)
+    mov $prompt_msg, %rsi          # address of the message to write
+    mov prompt_length, %rdx           # number of bytes to write (length of the message)
     syscall
 
         # Read user input into buffer
-    movq $0, %rax            # syscall: sys_read (0)
-    movq $0, %rdi            # file descriptor: stdin (0)
-    movq $buffer, %rsi       # address of the buffer to store the input
-    movq $100, %rdx          # number of bytes to read (max 100 bytes)
+    mov $0, %rax            # syscall: sys_read (0)
+    mov $0, %rdi            # file descriptor: stdin (0)
+    mov $buffer, %rsi       # address of the buffer to store the input
+    mov $100, %rdx          # number of bytes to read (max 100 bytes)
     syscall
 
        # Save input lenght to a register
-       movq %rax, %r12
+       mov %rax, %r12
 
         # Write the the greeting to stdout
-    movq $1, %rax            # syscall: sys_write (1)
-    movq $1, %rdi            # file descriptor: stdout (1)
-    movq $greeting_msg, %rsi          # address of the message to write
-    movq $7, %rdx           # number of bytes to write (length of the message)
+    mov $1, %rax            # syscall: sys_write (1)
+    mov $1, %rdi            # file descriptor: stdout (1)
+    mov $greeting_msg, %rsi          # address of the message to write
+    mov $7, %rdx           # number of bytes to write (length of the message)
     syscall
 
        # Write the the name to stdout
-    movq $1, %rax            # syscall: sys_write (1)
-    movq $1, %rdi            # file descriptor: stdout (1)
-    movq $buffer, %rsi          # address of the message to write
-    movq %r12, %rdx           # number of bytes to write (length of the message)
+    mov $1, %rax            # syscall: sys_write (1)
+    mov $1, %rdi            # file descriptor: stdout (1)
+    mov $buffer, %rsi          # address of the message to write
+    mov %r12, %rdx           # number of bytes to write (length of the message)
     syscall
 
 
 
         # Exit the program
-    movq $60, %rax           # syscall: sys_exit (60)
+    mov $60, %rax           # syscall: sys_exit (60)
     xor %rdi, %rdi           # exit code 0
     syscall
